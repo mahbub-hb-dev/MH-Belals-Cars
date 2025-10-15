ad4ebdce40835b01c49b45cb2d032db14d369968
Mahbub Hasan <mahbub.hb.dev@gmail.com>
2025-10-15 14:51:00 +0600

    fixed checkbox problem

--- src/ShowPremium.jsx
+++ src/ShowPremium.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ShowPremium = () => {
   return (
     <span className='max-md:block max-md:my-2.5'>
-        <input type="checkbox" /> 
-        <label className='ml-2'>Show Premium Cars</label>
+        <input type="checkbox" id='ispremium' /> 
+        <label htmlFor="ispremium"> Show premium cars only </label>
     </span>
   )
 }
